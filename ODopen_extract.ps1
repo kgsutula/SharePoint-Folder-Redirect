87c3fdbf9982d3cf5891a254f0b447c1b61bab99
kgsutula <kgsutula@gmail.com>
2026-06-11 21:46:22 -0700

    Add files via upload

---
+++ ODopen_extract.ps1
@@ -0,0 +1,29 @@
+﻿Add-Type -AssemblyName System.Web
+
+ 
+
+$syncUrl = Read-Host -Prompt "Please paste the Odopen link here"
+
+$uri = [System.Uri]$syncUrl
+
+$query = [System.Web.HttpUtility]::ParseQueryString($uri.Query)
+
+ 
+
+$siteId = $query["siteId"]
+
+$webId  = $query["webId"]
+
+$listId = $query["listId"]
+
+Write-Host "SiteID = $siteID"
+Write-Host "WebID = $WebID"
+Write-Host "listid = $listid"
+
+
+
+
+
+
+
+
